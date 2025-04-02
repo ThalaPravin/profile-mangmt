f5c62e88402f2b3574bc5f32c2de50bdce7b49ba
Pravin <pravinavhad3597@gmail.com>
2025-04-02 11:44:22 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+    <div className='bg-red-500 w-full h-screen'>
+
+    </div>
+  </>
+  )
+}
+
+export default App
